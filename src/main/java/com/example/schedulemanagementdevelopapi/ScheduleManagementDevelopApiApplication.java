8615631c9e3466006fb8f098c05405106325f20f
김영훈 <hunifive@gmail.com>
2025-08-06 20:48:24 +0900

    init

---
+++ src/main/java/com/example/schedulemanagementdevelopapi/ScheduleManagementDevelopApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.schedulemanagementdevelopapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleManagementDevelopApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleManagementDevelopApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/schedulemanagementdevelopapi/ScheduleManagementDevelopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.schedulemanagementdevelopapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleManagementDevelopApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
