d98a02d75bfc88face188a1a6d835a0790b0f9ca
김영훈 <hunifive@gmail.com>
2025-08-07 17:23:09 +0900

    fix: `entity`에 설정되어있던 `validation`을 `requestDto`로 변경

--- src/main/java/com/example/schedulemanagementdevelopapi/schedule/entity/Schedule.java
+++ src/main/java/com/example/schedulemanagementdevelopapi/schedule/entity/Schedule.java
@@ -2,8 +2,6 @@ package com.example.schedulemanagementdevelopapi.schedule.entity;
 
 import com.example.schedulemanagementdevelopapi.global.entity.BaseEntity;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -15,14 +13,10 @@ public class Schedule extends BaseEntity {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotBlank(message = "일정의 작성자명은 필수값입니다.")
     private String writer;
 
-    @NotBlank(message = "일정의 제목은 필수값입니다.")
-    @Size(max = 30, message = "일정 제목은 최대 30자입니다.")
     private String title;
 
-    @NotBlank(message = "일정의 내용은 필수값입니다.")
     @Column(columnDefinition = "longtext")
     private String content;
 
