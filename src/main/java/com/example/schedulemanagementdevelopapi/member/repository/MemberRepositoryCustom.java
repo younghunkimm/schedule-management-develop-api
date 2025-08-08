f8535758fbe1306faad7ffe9fa6d71c698a1fef6
김영훈 <hunifive@gmail.com>
2025-08-08 15:35:01 +0900

    feat: 유저 전체 조회 기능

--- src/main/java/com/example/schedulemanagementdevelopapi/member/repository/MemberRepositoryCustom.java
+++ src/main/java/com/example/schedulemanagementdevelopapi/member/repository/MemberRepositoryCustom.java
@@ -1,4 +1,11 @@
 package com.example.schedulemanagementdevelopapi.member.repository;
 
+import com.example.schedulemanagementdevelopapi.member.dto.request.MemberSearchConditionDto;
+import com.example.schedulemanagementdevelopapi.member.entity.Member;
+
+import java.util.List;
+
 public interface MemberRepositoryCustom {
+
+    List<Member> search(MemberSearchConditionDto cond);
 }
--- src/main/java/com/example/schedulemanagementdevelopapi/member/repository/MemberRepositoryImpl.java
+++ src/main/java/com/example/schedulemanagementdevelopapi/member/repository/MemberRepositoryImpl.java
@@ -1,13 +1,44 @@
 package com.example.schedulemanagementdevelopapi.member.repository;
 
-import com.example.schedulemanagementdevelopapi.schedule.entity.QSchedule;
+import com.example.schedulemanagementdevelopapi.member.dto.request.MemberSearchConditionDto;
+import com.example.schedulemanagementdevelopapi.member.entity.Member;
+import com.example.schedulemanagementdevelopapi.member.entity.QMember;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.springframework.util.StringUtils;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 public class MemberRepositoryImpl implements MemberRepositoryCustom {
 
     private final JPAQueryFactory queryFactory;
-    private final QSchedule schedule = QSchedule.schedule;
+    private final QMember qMember = QMember.member;
 
+    @Override
+    public List<Member> search(MemberSearchConditionDto cond) {
+
+        return queryFactory
+                .selectFrom(qMember)
+                .where(
+                        containsName(cond.getName()),
+                        containsEmail(cond.getEmail())
+                )
+                .where(qMember.deletedAt.isNull())
+                .orderBy(qMember.modifiedAt.desc())
+                .fetch();
+    }
+
+    private BooleanExpression containsName(String name) {
+        return (StringUtils.hasText(name))
+                ? qMember.name.containsIgnoreCase(name)
+                : null;
+    }
+
+    private BooleanExpression containsEmail(String email) {
+        return (StringUtils.hasText(email))
+                ? qMember.email.contains(email)
+                : null;
+    }
 }
