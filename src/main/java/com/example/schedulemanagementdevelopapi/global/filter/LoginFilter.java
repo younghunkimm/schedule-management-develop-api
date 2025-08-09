9eddfd02016118abd781e6c76b21881451704b10
김영훈 <hunifive@gmail.com>
2025-08-09 21:37:25 +0900

    feat(auth): 로그인 인증 필터

---
+++ src/main/java/com/example/schedulemanagementdevelopapi/global/filter/LoginFilter.java
@@ -0,0 +1,42 @@
+package com.example.schedulemanagementdevelopapi.global.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.PatternMatchUtils;
+
+import java.io.IOException;
+
+@Slf4j
+public class LoginFilter implements Filter {
+
+    private static final String[] WHITE_LIST = { "/", "/members/signup", "/members/login" };
+
+    @Override
+    public void doFilter(
+            ServletRequest servletRequest,
+            ServletResponse servletResponse,
+            FilterChain filterChain
+    ) throws IOException, ServletException {
+
+        HttpServletRequest httpRequest = (HttpServletRequest) servletRequest;
+        HttpServletResponse httpResponse = (HttpServletResponse) servletResponse;
+        String requestURI = httpRequest.getRequestURI();
+
+        if (false == isWhiteList(requestURI)) {
+            HttpSession session = httpRequest.getSession(false);
+            if (session == null || session.getAttribute("LOGIN_MEMBER") == null) {
+                httpResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, "로그인 해주세요.");
+                return;
+            }
+        }
+
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    private boolean isWhiteList(String requestURI) {
+        return PatternMatchUtils.simpleMatch(WHITE_LIST, requestURI);
+    }
+}
