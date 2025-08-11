6d82b9fc51944589e6c80b9dd252056bf91b63f1
김영훈 <hunifive@gmail.com>
2025-08-11 14:26:37 +0900

    fix(schedule): 일정 수정 시 권한 없을 때 에러메세지 수정

--- src/main/java/com/example/schedulemanagementdevelopapi/schedule/exception/ScheduleErrorCode.java
+++ src/main/java/com/example/schedulemanagementdevelopapi/schedule/exception/ScheduleErrorCode.java
@@ -11,7 +11,7 @@ public enum ScheduleErrorCode implements ErrorType {
 
     // 4xx
     SCHEDULE_NOT_FOUND(HttpStatus.NOT_FOUND, "SCHEDULE-001", "삭제되었거나 존재하지 않는 일정입니다."),
-    ACCESS_DENIED(HttpStatus.FORBIDDEN, "SCHEDULE-002", "삭제할 권한이 없습니다.");
+    ACCESS_DENIED(HttpStatus.FORBIDDEN, "SCHEDULE-002", "권한이 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
