fa50238d77bb8caa34b2d1240d1c7f2d1f431f14
김영훈 <hunifive@gmail.com>
2025-08-11 22:19:04 +0900

    refactor(schedule): 데이터 조회 후 유저 검증하도록 변경

--- src/main/java/com/example/schedulemanagementdevelopapi/schedule/entity/Schedule.java
+++ src/main/java/com/example/schedulemanagementdevelopapi/schedule/entity/Schedule.java
@@ -5,6 +5,7 @@ import com.example.schedulemanagementdevelopapi.member.entity.Member;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.util.ObjectUtils;
 
 @Entity
 @Getter
@@ -27,6 +28,10 @@ public class Schedule extends SoftDeletableEntity {
         this.member = member;
         this.title = title;
         this.content = content;
+    }
+
+    public boolean isOwnedBy(Long memberId) {
+        return ObjectUtils.nullSafeEquals(this.member.getId(), memberId);
     }
 
     public void updateTitle(String title) {
