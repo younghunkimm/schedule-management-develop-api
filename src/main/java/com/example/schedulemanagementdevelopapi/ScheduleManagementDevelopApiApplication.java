1cdeff145fac9db77206b670c879d167d0711798
김영훈 <hunifive@gmail.com>
2025-08-12 14:31:03 +0900

    fix(schedule): page 직렬화 전역 설정(pageImpl -> DTO 구조로 변환)

--- src/main/java/com/example/schedulemanagementdevelopapi/ScheduleManagementDevelopApiApplication.java
+++ src/main/java/com/example/schedulemanagementdevelopapi/ScheduleManagementDevelopApiApplication.java
@@ -3,9 +3,13 @@ package com.example.schedulemanagementdevelopapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class ScheduleManagementDevelopApiApplication {
 
     public static void main(String[] args) {
